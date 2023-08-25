e64f62897f7017bd42fd5533f88a139ede011766
r.golovko <r.golovko@owen.ua>
2023-08-25 12:37:43 +0300

    Додано проксі макет. відкориговано метод зміни ориентації макету

--- AQ_toolbar_group.py
+++ AQ_toolbar_group.py
@@ -13,16 +13,23 @@ class AQ_toolbar_group_template(QWidget):
         super().__init__(parent)
         self.buttons = []
         self.group_layout = 0
+        self.setMinimumSize(60, 60)
+        self.proxy_lay = QHBoxLayout(self)
+        self.proxy_lay.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.proxy_lay)
 
     def change_oriental(self):
-        if isinstance(self.layout(), QHBoxLayout):
-            self.layout().deleteLater()
+        cur_group_layout = self.proxy_lay.itemAt(0)
+        if isinstance(cur_group_layout, QHBoxLayout):
+            self.proxy_lay.removeItem(cur_group_layout)
             self.group_layout = Group_LayV(self, *self.buttons)
-            self.setLayout(self.group_layout)
-        elif isinstance(self.layout(), QVBoxLayout):
-            self.layout().deleteLater()
+            self.proxy_lay.addLayout(self.group_layout)
+            cur_group_layout.deleteLater()
+        elif isinstance(cur_group_layout, QVBoxLayout):
+            self.proxy_lay.removeItem(cur_group_layout)
             self.group_layout = Group_LayH(self, *self.buttons)
-            self.setLayout(self.group_layout)
+            self.proxy_lay.addLayout(self.group_layout)
+            cur_group_layout.deleteLater()
 
 
 class AQ_device_action_group(AQ_toolbar_group_template):
@@ -32,6 +39,7 @@ class AQ_device_action_group(AQ_toolbar_group_template):
         self.ico_btn_add_devise = QIcon(PROJ_DIR + 'Icons/Add_device.png')
         self.btn_add_devices = AQ_ToolButton('Add Devices', self.ico_btn_add_devise)
         # тут вставить привязку к функции self.btn_add_devices.clicked.connect(???)
+        # self.btn_add_devices.clicked.connect(self.change_oriental)
         self.buttons.append(self.btn_add_devices)
     # кнопка 2
         self.ico_btn_delete_device = QIcon(PROJ_DIR + 'Icons/Delete_device.png')
@@ -46,7 +54,7 @@ class AQ_device_action_group(AQ_toolbar_group_template):
 
     # Створюємо початковий горизонтальний лейаут
         self.group_layout = Group_LayH(self, *self.buttons)
-        self.setLayout(self.group_layout)
+        self.proxy_lay.addLayout(self.group_layout)
 
 
 class AQ_param_action_group(AQ_toolbar_group_template):
@@ -56,6 +64,7 @@ class AQ_param_action_group(AQ_toolbar_group_template):
         self.ico_btn_read = QIcon(PROJ_DIR + 'Icons/test_Button.png')
         self.btn_read = AQ_ToolButton('Read parameters', self.ico_btn_read)
         # тут вставить привязку к функции self.btn_add_devices.clicked.connect(???)
+        # self.btn_read.clicked.connect(self.change_oriental)
         self.buttons.append(self.btn_read)
     # кнопка 2
         self.ico_btn_write = QIcon(PROJ_DIR + 'Icons/test_Button.png')
@@ -75,51 +84,51 @@ class AQ_param_action_group(AQ_toolbar_group_template):
 
     # Створюємо початковий горизонтальний лейаут
         self.group_layout = Group_LayH(self, *self.buttons)
-        self.setLayout(self.group_layout)
+        self.proxy_lay.addLayout(self.group_layout)
 
 
 class AQ_utils_group(AQ_toolbar_group_template):
     def __init__(self, parent=None):
         super().__init__(parent)
     # кнопка 1
-        self.ico_btn_read = QIcon(PROJ_DIR + 'Icons/test_Button.png')
-        self.btn_read = AQ_ToolButton('Real-time clock', self.ico_btn_read)
+        self.ico_btn_rtc = QIcon(PROJ_DIR + 'Icons/test_Button.png')
+        self.btn_rtc = AQ_ToolButton('Real-time clock', self.ico_btn_rtc)
         # тут вставить привязку к функции self.btn_add_devices.clicked.connect(???)
-        self.buttons.append(self.btn_read)
+        # self.btn_rtc.clicked.connect(self.change_oriental)
+        self.buttons.append(self.btn_rtc)
     # кнопка 2
-        self.ico_btn_write = QIcon(PROJ_DIR + 'Icons/test_Button.png')
-        self.btn_write = AQ_ToolButton('Password', self.ico_btn_write)
+        self.ico_btn_pass = QIcon(PROJ_DIR + 'Icons/test_Button.png')
+        self.btn_pass = AQ_ToolButton('Password', self.ico_btn_pass)
         # тут вставить привязку к функции self.btn_add_devices.clicked.connect(???)
-        self.buttons.append(self.btn_write)
+        self.buttons.append(self.btn_pass)
     # кнопка 3
-        self.ico_btn_factory_settings = QIcon(PROJ_DIR + 'Icons/test_Button.png')
-        self.btn_factory_settings = AQ_ToolButton('Calibration', self.ico_btn_factory_settings)
+        self.ico_btn_calib = QIcon(PROJ_DIR + 'Icons/test_Button.png')
+        self.btn_calib = AQ_ToolButton('Calibration', self.ico_btn_calib)
         # тут вставить привязку к функции self.btn_add_devices.clicked.connect(???)
-        self.buttons.append(self.btn_factory_settings)
+        self.buttons.append(self.btn_calib)
 
     # Створюємо початковий горизонтальний лейаут
         self.group_layout = Group_LayH(self, *self.buttons)
-        self.setLayout(self.group_layout)
+        self.proxy_lay.addLayout(self.group_layout)
 
 
 class AQ_archieve_group(AQ_toolbar_group_template):
     def __init__(self, parent=None):
         super().__init__(parent)
     # кнопка 1
-        self.ico_btn_read = QIcon(PROJ_DIR + 'Icons/test_Button.png')
-        self.btn_read = AQ_ToolButton('Save log data', self.ico_btn_read)
+        self.ico_btn_save_log = QIcon(PROJ_DIR + 'Icons/test_Button.png')
+        self.btn_save_log = AQ_ToolButton('Save log data', self.ico_btn_save_log)
         # тут вставить привязку к функции self.btn_add_devices.clicked.connect(???)
-        self.buttons.append(self.btn_read)
+        self.buttons.append(self.btn_save_log)
     # кнопка 2
-        self.ico_btn_write = QIcon(PROJ_DIR + 'Icons/test_Button.png')
-        self.btn_write = AQ_ToolButton('Data logging settings', self.ico_btn_write)
+        self.ico_btn_log_settings = QIcon(PROJ_DIR + 'Icons/test_Button.png')
+        self.btn__log_settings = AQ_ToolButton('Data logging settings', self.ico_btn_log_settings)
         # тут вставить привязку к функции self.btn_add_devices.clicked.connect(???)
-        self.buttons.append(self.btn_write)
+        self.buttons.append(self.btn__log_settings)
 
     # Створюємо початковий горизонтальний лейаут
         self.group_layout = Group_LayH(self, *self.buttons)
-        self.setLayout(self.group_layout)
-
+        self.proxy_lay.addLayout(self.group_layout)
 
 
 
