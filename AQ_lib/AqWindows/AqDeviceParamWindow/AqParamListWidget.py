c3ab96d494c3a85ba31af2ac55fc21508e062e40
Spy <yevhenii.filatov@gmail.com>
2023-11-28 01:25:57 +0200

    Added base classes for AqParamListWindow

--- AQ_lib/AqWindows/AqDeviceParamWindow/AqParamListWidget.py
+++ AQ_lib/AqWindows/AqDeviceParamWindow/AqParamListWidget.py
@@ -1,11 +1,49 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QFrame, QTableWidget
 
 from DeviceModels import AqDeviceParamListModel
 
+
 class AqParamListWidget(QWidget):
-    def __init__(self, dev_info: AqDeviceParamListModel = None):
-        if dev_info is None:
-            dev_info = generateTestData();
+    def __init__(self, _ui, dev_info: AqDeviceParamListModel = None, parent=None):
+        super().__init__(parent)
+        self.ui = _ui()
+        self.ui.setupUi(self)
+
+
+class AqParamListInfoFrame(QFrame):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+
+class AqParamListTableWidget(QTableWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+    """Base item list class
+    provide functionality by add data to table, set size, resize, etc"""
+
+
+class AqModbusParamListTableWidget(AqParamListTableWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+    """Here we will parse only items with ModbusItem type
+    this class will require some attributes from items"""
+
+
+class AqOwenParamListTableWidget(AqParamListTableWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+    """Here we will parse only items with OwenItem type
+    this class will require some attributes from items"""
+
+
+class AqTestParamListTableWidget(AqParamListTableWidget):
+    def __init__(self, parent=None, rows=None, columns=None, maxItemLen=None):
+        super().__init__(parent)
+    """This class generate data for test AqParamListTableWidget with different
+    count rows and columns"""
+
+    def generateData(self):
+        pass
 
 
 
