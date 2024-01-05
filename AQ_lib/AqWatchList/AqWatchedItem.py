fb82c337e5c4e28c72d89b7b93a0bcf1068632d5
romangolovkou <romangolovkou@gmail.com>
2024-01-05 09:38:52 +0200

    додано фільтр на повторне додавання ітему у watchCore (при спробі додати все доданий ітем, не додається)

--- AQ_lib/AqWatchList/AqWatchedItem.py
+++ AQ_lib/AqWatchList/AqWatchedItem.py
@@ -10,4 +10,5 @@ class WatchedItem:
         self.device: AqBaseDevice = device
 
     def addItemToWatch(self, item):
-        self.items.append(item)
+        if item not in self.items:
+            self.items.append(item)
