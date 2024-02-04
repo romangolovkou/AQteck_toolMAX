0e61541d7e155c5b61bc389b0addb4e9dce6a726
romangolovkou <romangolovkou@gmail.com>
2024-02-04 20:17:57 +0200

    Видалення зайвого

--- AQ_lib/AQ_Devices/DeviceItems/AqAutoDetectionItems.py
+++ AQ_lib/AQ_Devices/DeviceItems/AqAutoDetectionItems.py
@@ -219,17 +219,11 @@ class AqAutoDetectModbusFileItem(AqModbusFileItem):
             high = 0
 
             password_bytes = userPass.encode('cp1251')
-            password_bytes_hex = password_bytes.hex()
 
             for i in range(0, len(password_bytes), 2):
-                # low_b = password_bytes[i]
-                # print('low_b = ' + low_b.to_bytes(5, byteorder='big', signed=True).hex())
                 low += password_bytes[i]
-                # print('low_before<<>> = ' + low.to_bytes(5, byteorder='big', signed=True).hex())
                 low -= (low << 13) | (low >> 19)
                 low = low & 0xFFFFFFFF
-                low_len = low.bit_length()
-                print('low_i = ' + low.to_bytes(5, byteorder='big', signed=True).hex())
 
                 if i + 1 >= len(password_bytes):
                     break
@@ -237,20 +231,11 @@ class AqAutoDetectModbusFileItem(AqModbusFileItem):
                 high += password_bytes[i + 1]
                 high -= (high << 13) | (high >> 19)
                 high = high & 0xFFFFFFFF
-                print('high_i = ' + high.to_bytes(5, byteorder='big', signed=True).hex())
 
-            low_res = low.to_bytes(5, byteorder='big', signed=True)
-            high_res = high.to_bytes(5, byteorder='big', signed=True)
-            # res = low.to_bytes(4, byteorder='big', signed=True) + high.to_bytes(4, byteorder='big', signed=True)
-            low_res_hex = low_res.hex()
-            high_res_hex = high_res.hex()
-            res = low + high
-            print('res = ' + res.to_bytes(5, byteorder='big', signed=True).hex())
             low = bytes.fromhex(hex(low)[2:])[::-1]
             high = bytes.fromhex(hex(high)[2:])[::-1]
             hash = low + high
             return hash
-            # return b"\x9E\xE0\xBD\xDF\xA8\x98\xBA\x63"
 
 
     @property
