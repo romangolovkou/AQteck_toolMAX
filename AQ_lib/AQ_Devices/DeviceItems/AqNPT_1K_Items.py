a1569714715b7872f89daea9bcc0f508f9e1fb6b
romangolovkou <romangolovkou@gmail.com>
2024-02-16 11:36:39 +0200

    Зроблено костиль, що дозволяє калібрувати НПТ

--- AQ_lib/AQ_Devices/DeviceItems/AqNPT_1K_Items.py
+++ AQ_lib/AQ_Devices/DeviceItems/AqNPT_1K_Items.py
@@ -1,7 +1,7 @@
 import struct
 
 from AqBaseTreeItems import AqStringParamItem, AqModbusItem, AqFloatParamItem, AqMACParamItem
-from AqModbusTips import remove_empty_bytes
+from AqModbusTips import remove_empty_bytes, swap_registers
 
 
 class AqNPT_1K_StringParamItem(AqStringParamItem, AqModbusItem):
@@ -84,6 +84,51 @@ class AqNPT_1K_CalibResultParamItem(AqStringParamItem, AqModbusItem):
         # byte_array = swap_bytes_at_registers(byte_array, reg_count)
         # Расшифровуем в строку
         # text = byte_array.decode('ANSI')
+        param_value = text #remove_empty_bytes(text)
+
+        return param_value
+
+
+class AqNPT_1K_CalibrCommandParamItem(AqStringParamItem, AqModbusItem):
+    def __init__(self, param_attributes):
+        param_attributes['type'] = 'string'
+        super().__init__(param_attributes)
+
+    def pack(self):
+        command_str = self.value
+
+        fields = command_str.split(':')
+        float_value = float(fields[0])
+        command = int(fields[1])
+        # Преобразование в байты
+        float_bytes = struct.pack('f', float_value)
+        command_bytes = struct.pack('I', command)
+
+
+        # Объединение в один массив байт
+        combined_bytes = float_bytes + command_bytes
+
+        # text_bytes = self.value.encode('ANSI')
+        #
+        # # Добавляем нулевой байт в конец, если длина списка не кратна 2
+        # if len(text_bytes) % 2 != 0:
+        #     text_bytes += b'\x00'
+        combined_bytes = swap_registers(combined_bytes)
+
+        registers = [struct.unpack('H', combined_bytes[i:i + 2])[0] for i in range(0, len(combined_bytes), 2)]
+
+        return registers
+
+    def unpack(self, data):
+        # Конвертируем значения регистров в строку
+        hex_string = ''.join(format(value, '04X') for value in data.registers)
+        # Конвертируем строку в массив байт
+        byte_array = bytes.fromhex(hex_string)
+
+        reg_count = self.param_size // 2
+        # byte_array = swap_bytes_at_registers(byte_array, reg_count)
+        # Расшифровуем в строку
+        text = byte_array.decode('ANSI')
         param_value = text #remove_empty_bytes(text)
 
         return param_value
