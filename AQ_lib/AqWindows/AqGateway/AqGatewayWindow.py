f3b2cdce10d417baa83513395edf5acabac4c3a3
romangolovkou <romangolovkou@gmail.com>
2024-03-21 18:21:53 +0200

    Поправлено скролл бар, тв додано єдітор для порту

--- AQ_lib/AqWindows/AqGateway/AqGatewayWindow.py
+++ AQ_lib/AqWindows/AqGateway/AqGatewayWindow.py
@@ -25,12 +25,12 @@ class AqGatewayWindow(AqDialogTemplate):
         # Устанавливаем ширину столбцов в таблице справа
         cur_width = self.ui.tableWidget.width()
         self.ui.tableWidget.setColumnWidth(0, int(cur_width * 0.05))
-        self.ui.tableWidget.setColumnWidth(1, int(cur_width * 0.18))
+        self.ui.tableWidget.setColumnWidth(1, int(cur_width * 0.17))
         self.ui.tableWidget.setColumnWidth(2, int(cur_width * 0.08))
         self.ui.tableWidget.setColumnWidth(3, int(cur_width * 0.28))
-        self.ui.tableWidget.setColumnWidth(4, int(cur_width * 0.18))
-        self.ui.tableWidget.setColumnWidth(5, int(cur_width * 0.18))
-        self.ui.tableWidget.setColumnWidth(6, int(cur_width * 0.05))
+        self.ui.tableWidget.setColumnWidth(4, int(cur_width * 0.17))
+        self.ui.tableWidget.setColumnWidth(5, int(cur_width * 0.17))
+        self.ui.tableWidget.setColumnWidth(6, int(cur_width * 0.06))
         self.ui.tableFrame.hide()
         self.ui.addDeviceBtn.hide()
 
@@ -132,14 +132,23 @@ class AqGatewayTableWidget(QTableWidget):
         self.setRowCount(0)
         self.setFixedWidth(420)
         self.setMaximumHeight(420)
+        self.verticalScrollBar().rangeChanged.connect(self.on_vertical_scrollbar_range_changed)
         self.horizontalHeader().setStyleSheet(
             "QHeaderView::section { background-color: #2b2d30; color: #D0D0D0; border: 1px solid #1e1f22; }")
         # Убираем рамку таблицы
         self.setStyleSheet("""QTableWidget { border: none; color: #D0D0D0; background-color: #16191d;}
                             QTableWidget::item { padding-left: 3px; }
                             QTableWidget::item:!focus {
-                            background-color: transparent; 
-                            }""")
+                            background-color: transparent;}
+                            QScrollBar:vertical {
+                                background: #1e1f22;
+                                width: 10px;  /* Ширина вертикального скроллбара */
+                            }
+                            QScrollBar:horizontal {
+                                background: #1e1f22;
+                                height: 10px;  /* Высота горизонтального скроллбара */
+                            }
+                            """)
         self.cellClicked.connect(self.cell_clicked)
 
     def cell_clicked(self, row, col):
@@ -152,6 +161,8 @@ class AqGatewayTableWidget(QTableWidget):
     def append_row(self):
         # if new_row_index is None:
         new_row_index = self.rowCount()
+        if new_row_index == 31:
+            return
         self.setRowCount(self.rowCount() + 1)
         # Создаем элементы таблицы для каждой строки
         # number_item = QTableWidgetItem(str(new_row_index + 1))
@@ -181,7 +192,7 @@ class AqGatewayTableWidget(QTableWidget):
         out_ip_address_item = AqIpGatewayLineEdit(self)
         out_ip_address_item.setAlignment(Qt.AlignmentFlag.AlignCenter)
         out_ip_address_item.setText(prev_ip)
-        out_ip_port_item = AqSlaveIdLineEdit(self)
+        out_ip_port_item = AqPortGatewayLineEdit(self)
         out_ip_port_item.setAlignment(Qt.AlignmentFlag.AlignCenter)
         out_ip_port_item.setText('502')
         # out_slave_id = QTableWidgetItem('1')
@@ -193,6 +204,7 @@ class AqGatewayTableWidget(QTableWidget):
         icon = QIcon('UI/icons/trash.png')
         # item = self.item(new_row_index, 6)
         delete_btn_item.setIcon(icon)
+        delete_btn_item.setTextAlignment(Qt.AlignCenter)
 
         # Устанавливаем элементы таблицы
         self.setCellWidget(new_row_index, 0, number_item)
@@ -207,6 +219,8 @@ class AqGatewayTableWidget(QTableWidget):
         for i in range(self.rowCount()):
             self.cellWidget(i, 0).setText(str(i + 1))
 
+    def on_vertical_scrollbar_range_changed(self, minimum, maximum):
+        pass
 
 class AqSlaveIdGatewayLineEdit(AqSlaveIdLineEdit):
     def __init__(self, parent=None):
@@ -274,3 +288,11 @@ class AqIpGatewayLineEdit(AqIpLineEdit):
         # self.err_label.show()
         # # Устанавливаем задержку в 2 секунды и затем удаляем метку
         # QTimer.singleShot(3000, self.err_label.deleteLater)
+
+
+class AqPortGatewayLineEdit(AqSlaveIdGatewayLineEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.min_limit = 0
+        self.max_limit = 65535
+        self.max_str_len = 5
