177e3371fe86d22ac043e621e1e639f91d5d76c2
Roman <you@example.com>
2026-06-08 16:36:43 +0300

    Додано бат та iss для збірки патчу для OfflineAutoDetect

---
+++ Build installer/cx_Freeze/Build AutoDetect PatchPack.bat
@@ -0,0 +1,13 @@
+set INNO="c:\Program Files (x86)\Inno Setup 6\Compil32.exe" /cc
+
+rem set /p VER=<..\..\Version.txt
+rem set VER=0.0.2
+set VER=0.1.0_AutoDetect
+
+echo load version string %VER%
+
+cat AQteck_tool_MAX_AutoDetect_PatchPack.iss | sed s/__VERSION__/%VER%/g > AQteck_tool_MAX_AutoDetect_PatchPack_with_VERSION.iss
+
+set ISS="AQteck_tool_MAX_AutoDetect_PatchPack_with_VERSION.iss"
+
+%INNO% %ISS%
